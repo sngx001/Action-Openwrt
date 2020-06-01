2f563645ef08fccacd4d851d1f4ccfcff9039596
hyird <31695447+hyird@users.noreply.github.com>
2020-06-01 15:09:04 +0800

    Update custom.sh

--- user/lean-mt7621/custom.sh
+++ user/lean-mt7621/custom.sh
@@ -9,7 +9,7 @@ sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-set
 sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
 sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
 
-git clone https://github.com/hyird/passwall.git                     package/passwall
+git clone https://github.com/hyird/pw.git -b master                     package/passwall
 git clone https://github.com/Mrbai98/luci-theme-atmaterial.git      package/luci-theme-atmaterial
 svn checkout https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/adg
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
