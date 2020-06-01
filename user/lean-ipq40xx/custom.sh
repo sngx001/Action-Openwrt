1980a501685ceabbbe89b05766f69cfff5bcfec1
hyird <31695447+hyird@users.noreply.github.com>
2020-06-01 11:34:17 +0800

    Update custom.sh

--- user/lean-ipq40xx/custom.sh
+++ user/lean-ipq40xx/custom.sh
@@ -7,7 +7,7 @@ sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-d
 sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/lean/default-settings/files/zzz-default-settings
 sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
 sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
-sed -i "s/R20\(.[0-9].[0-9][0-9]\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
+sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
 
 git clone https://github.com/hyird/passwall.git                     package/passwall
 git clone https://github.com/Mrbai98/luci-theme-atmaterial.git      package/luci-theme-atmaterial
