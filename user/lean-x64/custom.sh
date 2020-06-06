0d53d8a4a159017876b91f06865dd9542d09346f
abc <abc@qq.com>
2020-06-06 13:07:40 +0800

    add x86_64

---
+++ user/lean-x64/custom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Test custom.sh"
+
+source ../version
+sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
+sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
+
+git clone https://github.com/hyird/pw.git -b master                     package/passwall
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git      package/luci-theme-atmaterial
+svn checkout https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/adg
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+git clone https://github.com/project-openwrt/openwrt-gowebdav.git package/webdav
