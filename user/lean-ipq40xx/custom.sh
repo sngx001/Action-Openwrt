54647e74d9afaac01a99ed40e660cbcd6633d7c2
hyird <31695447+hyird@users.noreply.github.com>
2020-06-09 12:49:30 +0800

    Update custom.sh

--- user/lean-ipq40xx/custom.sh
+++ user/lean-ipq40xx/custom.sh
@@ -3,6 +3,7 @@
 echo "Test custom.sh"
 
 source ../version
+sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/lean/default-settings/files/zzz-default-settings
 sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
