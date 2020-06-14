227ff8d15111eb30f95f17e3d5fff5b9d1de8434
hyird <31695447+hyird@users.noreply.github.com>
2020-06-14 19:58:36 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -10,6 +10,7 @@ sed -i "s/#sed/sed/g"                package/default-settings/files/zzz-default-
 sed -i "s/openwrt.proxy.ustclug.org/op.hyird.xyz/g"  package/default-settings/files/zzz-default-settings
 sed -i "s/https/L20.$version/g"  package/default-settings/files/zzz-default-settings
 sed -i  's/http/releases\\\/19.07\-SNAPSHOT/g'  package/default-settings/files/zzz-default-settings
+echo "sed \"s/19.07-SNAPSHOT/L20.\$version/g\" /etc/openwrt_release "
 
 git clone https://github.com/fw876/helloworld.git  package/ssr
 git clone https://github.com/hyird/pw.git -b master   package/passwall
