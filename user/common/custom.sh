8e613f559386d83eb506c778b537ee589726e2b6
hyird <you@example.com>
2020-07-03 22:24:20 +0800

    add mt7621

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -21,6 +21,12 @@ svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2  
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping                             package/tcpping
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks                          package/microsocks
 git clone https://github.com/project-openwrt/openwrt-gowebdav.git                                        package/webdav
+git clone https://github.com/DNSCrypt/dnscrypt-proxy.git						     package/dnscrypt-proxy
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git                             package/luci-theme-infinityfreedom
+git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
+git clone https://github.com/peter-tank/luci-app-dnscrypt-proxy2.git                                     package/luci-app-dnscrypt-proxy2
+
+
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd				     package/luci-app-ttyd
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2                         package/luci-app-airplay2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-familycloud                      package/luci-app-familycloud               
