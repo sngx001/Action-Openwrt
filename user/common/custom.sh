ff49df2bd89720063b0b69db66dfe273fa84169a
hyird <31695447+hyird@users.noreply.github.com>
2020-07-14 16:56:47 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -28,14 +28,3 @@ git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git    
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial                          
 
 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2                         package/luci-app-airplay2
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-familycloud                      package/luci-app-familycloud                 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ps3netsrv                        package/luci-app-ps3netsrv
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ps3netsrv                                 package/ps3netsrv 
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/rclone-ng                                 package/rclone-ng 
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/rclone-webui-react                        package/rclone-webui-react
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/rclone                                    package/rclone
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-rclone                           package/luci-app-rclone
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/wol                                       package/wol
-
-
