51419532fec9bd510c7bf8fb4289f62a0126a76b
hyird <hyird@outlook.com>
2020-08-08 10:09:39 +0800

    x

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -3,6 +3,7 @@
 echo "Test custom.sh"
 
 source ../version
+sed -i '92d'                                                                   package/system/opkg/Makefile
 sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
@@ -15,6 +16,7 @@ git clone https://github.com/CCnut/feed-netkeeper.git -b openwrt-18.06          
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git                             package/luci-theme-infinityfreedom
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
 git clone https://github.com/destan19/OpenAppFilter.git                                                  package/oaf
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
 svn co https://github.com/Lienol/openwrt-luci/trunk/applications/luci-app-smartdns                       package/luci-app-smartdns
 
