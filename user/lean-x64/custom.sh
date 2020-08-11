023b1314ed6d9576c1704c906fb6ee2cdba54e94
hyird <hyird@outlook.com>
2020-08-11 19:00:13 +0800

    x

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -4,14 +4,12 @@ echo "Test custom.sh"
 
 source ../version
 sed -i '92d'                                                                   package/system/opkg/Makefile
-sed -i '32,34d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
-#sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
-#sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
-#sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
 sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
-sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" .config
 sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
 
 rm -rf package/lean/luci-theme-argon
