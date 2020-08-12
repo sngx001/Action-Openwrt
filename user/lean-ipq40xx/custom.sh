279788f9eb6fcf6972b008b364c7beadbbe19879
hyird <31695447+hyird@users.noreply.github.com>
2020-08-12 18:39:18 +0800

    Update custom.sh

--- user/lean-ipq40xx/custom.sh
+++ user/lean-ipq40xx/custom.sh
@@ -10,7 +10,7 @@ sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-se
 sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
 sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
 sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
-sed 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
+sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
 
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/CCnut/feed-netkeeper.git -b openwrt-18.06                                   package/nk
@@ -18,6 +18,9 @@ git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git    
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
 git clone https://github.com/destan19/OpenAppFilter.git                                                  package/oaf
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon
+git clone https://github.com/jerrykuku/node-request.git                                                  package/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git                                        package/luci-app-jd-dailybonus
+git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
+git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
 svn co https://github.com/Lienol/openwrt-luci/trunk/applications/luci-app-smartdns                       package/luci-app-smartdns
-
